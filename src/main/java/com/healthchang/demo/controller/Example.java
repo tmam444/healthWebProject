0b51df3ab811a3f98b64d680be9d10b2771b869b
이충렬 <ichunglyeol@ichunglyeol-ui-MacBookPro.local>
2020-04-14 17:34:02 +0900

    first commit

---
+++ src/main/java/com/healthchang/demo/controller/Example.java
@@ -0,0 +1,25 @@
+package com.healthchang.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Example {
+
+    @GetMapping("/")
+    String home(String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/test")
+    String test(){
+        return "Test!!";
+    }
+
+}
---
+++ src/main/java/com/healthchang/demo/domain/Member.java
@@ -0,0 +1,28 @@
+package com.healthchang.demo.domain;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Length(min = 2)
+    @Length(max = 20)
+    @Email
+    @NotBlank
+    private String username;
+
+    @Length(min = 2)
+    @NotBlank
+    private String password;
+
+}
