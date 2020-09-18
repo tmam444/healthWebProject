d77dbbcbec07a93cb1ba3df63b049badfa3c68e2
tmam444 <48037775+tmam444@users.noreply.github.com>
2020-09-18 23:15:11 +0900

    Access Token 받기 처리 완료

--- src/main/java/com/healthchang/demo/controller/AuthController.java
+++ src/main/java/com/healthchang/demo/controller/AuthController.java
@@ -1,30 +1,63 @@
 package com.healthchang.demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.healthchang.demo.config.auth.dto.OAuthToken;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class AuthController {
 
-    //Link
-    @GetMapping("/goNaverLogin")
-    public String goNaverLogin() {
-        return "APIExamNaverLogin";
-    }
-
-    @GetMapping("/auth/naver/callback")
-    public String naverLoginCallBack() {
-        return "callback";
-    }
-
     //Callback
-    @GetMapping("/auth/kakao/callback")
-    public @ResponseBody String kakaoLoginCallBack(){
-        return "카카오 인증 완료";
+    @GetMapping("/auth/code/kakao")
+    public @ResponseBody String kakaoLoginCallBack(String code){
+
+        //POST방식으로 key=value 데이터를 요청 (카카오쪽으로)
+        RestTemplate rt = new RestTemplate();
+
+        //HttpHeader 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //HttpBody 오브젝트 생성
+        MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
+        param.add("grant_type", "authorization_code");
+        param.add("client_id", "2c1e6cc30a132b4647e201a908fa932c");
+        param.add("redirect_uri", "http://127.0.0.1:8080/auth/code/kakao");
+        param.add("client_secret", "NL7SgK8FEv16yGPoXXBct0myw7yiYxSH");
+        param.add("code", code);
+
+        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(param, headers);
+
+        //Http 요청하기 - Post 방식으로 - 그리고response 변수의 응답 받음.
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        //Gson, Json Simple, ObjectMapper
+        ObjectMapper obMapper = new ObjectMapper();
+        OAuthToken oAuthToken = null;
+        try {
+            oAuthToken = obMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(oAuthToken.getAccess_token());
+        
+        return "카카오 인증 완료, 코드값 : " + response;
     }
-
-
-
-
 }
