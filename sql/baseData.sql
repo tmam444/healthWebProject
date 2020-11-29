ee83b2e1d8f2607af0d9bb950702a04720a813f9
tmam444 <48037775+tmam444@users.noreply.github.com>
2020-11-29 21:21:18 +0900

    SQL 파일 추가

---
+++ sql/baseData.sql
@@ -0,0 +1,25 @@
+insert into exercise_calories(name, calorie) value('배구', 4.84);
+insert into exercise_calories(name, calorie) value('골프', 5.06);
+insert into exercise_calories(name, calorie) value('스키', 5.72);
+insert into exercise_calories(name, calorie) value('자전거', 5.94);
+insert into exercise_calories(name, calorie) value('등산', 7.26);
+insert into exercise_calories(name, calorie) value('수영', 7.70);
+insert into exercise_calories(name, calorie) value('축구', 9.45);
+insert into exercise_calories(name, calorie) value('농구', 8.29);
+insert into exercise_calories(name, calorie) value('조깅', 9.24);
+insert into exercise_calories(name, calorie) value('야구', 5.25);
+insert into exercise_calories(name, calorie) value('버피테스트', 9.1);
+insert into exercise_calories(name, calorie) value('계단 오르기', 4.25);
+insert into exercise_calories(name, calorie) value('윗몸 일으키기', 4.31);
+insert into exercise_calories(name, calorie) value('등산', 6.81);
+insert into exercise_calories(name, calorie) value('스쿼트', 5.68);
+insert into exercise_calories(name, calorie) value('요가', 4.55);
+insert into exercise_calories(name, calorie) value('줄넘기', 12.5);
+insert into exercise_calories(name, calorie) value('아령', 4.2);
+insert into exercise_calories(name, calorie) value('걷기', 3.98);s
+
+select * from exercise_calories;
+
+
+
+
